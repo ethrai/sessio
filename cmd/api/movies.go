a63410d71e68eb2c66f98986fb39e3274069aa7e
Sergei Ryzhko <ryzhkosergej@gmail.com>
2024-10-09 22:55:19 +0300

    feat(api): write dummy json response

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"time"
+
+	"purego-api/internal/data"
 )
+
+type M map[string]any
 
 func (app *application) getMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
@@ -12,7 +16,23 @@ func (app *application) getMovieHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	fmt.Fprintf(w, "show the detail for movie with id %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		Title:     "Spider Man Across the Universe",
+		Runtime:   102,
+		Year:      2023,
+		Genres:    []string{"comics", "marvel", "something else"},
+		Version:   1,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	err = app.JSON(w, M{"movie": movie}, http.StatusOK, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "server error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
